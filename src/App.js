938be792d4a71ce1a42ced7d2d4aa7d77e416665
Jack Slater <JSLATE201@caledonian.ac.uk>
2022-06-30 09:56:59 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.css';
-import MusicContainer from './containers/MusicContainer';
+import React from 'react';
+import ChartContainer from './containers/ChartContainer';
 
 function App() {
+  const genres = [
+    {name: "All", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"},
+    {name: "Rock", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json"},
+    {name: "Dance", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json"},
+    {name: "Country", url: "https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json"}
+  ]
+  
   return (
-    <>
-      <h1>Top 20 Song Chart</h1>
-      <MusicContainer/>
-    </>
+    <ChartContainer genres={genres} />
   );
 }
 
